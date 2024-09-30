806e9bd255ca2d39109354d7623a031bb528b353
Samuel Goesniady <100010379+Samgoesniady@users.noreply.github.com>
2024-09-30 16:01:38 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -86,7 +86,7 @@ col3, col4, col5 = st.columns([1, 10, 1])
 
 
 with col3:
-    st.image('UNPATTI2.png', width=130)
+    st.image('UNPATTI2.jpg', width=130)
 
 with col4:
     st.markdown(
@@ -106,7 +106,7 @@ with col4:
         unsafe_allow_html=True
     )
 with col5:
-    st.image('UNPATTI.png', width=130)
+    st.image('UNPATTI.jpg', width=130)
 
 try:
     col6, col7 = st.columns([20, 20])
@@ -619,4 +619,4 @@ except ValueError:
 col12, col13, col14, col15, col16 = st.columns (5)
 
 with col13:
-    st.image('lisensi.PNG',width=900)
+    st.image('lisensi.jpg',width=900)
